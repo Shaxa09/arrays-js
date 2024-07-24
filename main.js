057ab79a7213b6978f3724ff97ac98e773ee1d30
Shaxa09 <shaxaraxmatullaev@gmail.com>
2024-07-24 16:21:59 +0500

    Filters

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// let students = ["Davlat", "Feruz", "Asad", "Danya"]
 
 // let nameToDelete = prompt("Введите имя для удаления: " + students.join(", "));
 
@@ -10,6 +9,7 @@
 //     alert("Имя не найдено в списке.");
 // }
 
+// -Callback-
 
 //(name) => name[0] === "D"
 // console.log(
@@ -18,7 +18,33 @@
 //     students.map((name) => name[0] === "D")
 //     students.forEach((name) => name[0] === "D")
 // )
-// find - vozvrashyaet samiy perviy kotoriy nnayder
+// find - vozvrashyaet samiy perviy kotoriy naydet
 // filter - vozvrashayet vse v massive
 // map - vozvrashyaet true ili false
 // forEach - vozvrashyaet underfined
+
+// const students = ["Davlat", "Feruz", "Asad", "Danya"]
+
+// const callback = (name) => name.typeof()
+
+// console.log(
+    //     arr.find(callback)
+    // )
+
+
+
+    const arr = ['Feruz', 22, true, null];
+    let str = [];
+    let num = [];
+    let bool = [];
+    let obj = [];
+    
+    str = arr.filter(element => typeof element === 'string');
+    num = arr.filter(element => typeof element === 'number');
+    bool = arr.filter(element => typeof element === 'boolean');
+    obj = arr.filter(element => typeof element === 'object');
+    
+    console.log(str);
+    console.log(num);
+    console.log(bool);
+    console.log(obj);
