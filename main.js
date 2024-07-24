fd9786fc137e0a25b48a8487cd1f31ed1ba073b7
Shaxa09 <shaxaraxmatullaev@gmail.com>
2024-07-24 15:38:54 +0500

    For-each_Js-cycles

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-let students = ["Davlat", "Feruz", "Asad", "Danya"]
+// let students = ["Davlat", "Feruz", "Asad", "Danya"]
 
-let nameToDelete = prompt("Введите имя для удаления: " + students.join(", "));
+// let nameToDelete = prompt("Введите имя для удаления: " + students.join(", "));
 
-if (students.includes(nameToDelete)) {
-    let index = students.indexOf(nameToDelete);
-    students.splice(index, 1);
-    alert("Обновленный список: " + students.join(", "));
-} else {
-    alert("Имя не найдено в списке.");
-}
+// if (students.includes(nameToDelete)) {
+//     let index = students.indexOf(nameToDelete);
+//     students.splice(index, 1);
+//     alert("Обновленный список: " + students.join(", "));
+// } else {
+//     alert("Имя не найдено в списке.");
+// }
+
+
+//(name) => name[0] === "D"
+// console.log(
+//     students.find((name) => name[0] === "D")
+//     students.filter((name) => name[0] === "D")
+//     students.map((name) => name[0] === "D")
+//     students.forEach((name) => name[0] === "D")
+// )
+// find - vozvrashyaet samiy perviy kotoriy nnayder
+// filter - vozvrashayet vse v massive
+// map - vozvrashyaet true ili false
+// forEach - vozvrashyaet underfined
